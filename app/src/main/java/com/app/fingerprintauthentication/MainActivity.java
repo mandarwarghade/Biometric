8dc81157755dacb89b4973d723aedada39449d70
mandarwarghade <47137785+mandarwarghade@users.noreply.github.com>
2020-08-29 01:17:09 +0530

    Initial commit

---
+++ app/src/main/java/com/app/fingerprintauthentication/MainActivity.java
@@ -0,0 +1,75 @@
+package com.app.fingerprintauthentication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+        import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+        import java.util.concurrent.Executors;
+
+        import androidx.biometric.BiometricPrompt;
+        import androidx.fragment.app.FragmentActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private void notifyUser(String message) {
+        Toast.makeText(this,
+                message,
+                Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Executor executor = Executors.newSingleThreadExecutor();
+
+        FragmentActivity activity = this;
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(activity, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                    // user clicked negative button
+                } else {
+                    //TODO: Called when an unrecoverable error has been encountered and the operation is complete.
+                }
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Intent i= new Intent(MainActivity.this, SecretActivity.class);
+                startActivity(i);
+                //TODO: Called when a biometric is recognized.
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                //TODO: Called when a biometric is valid but not recognized.
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Demo")
+                .setSubtitle("Authentication is required to continue")
+                .setDescription("This app uses biometric authentication to protect your data.")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        findViewById(R.id.btn_authenticate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
+
+    }
+}
